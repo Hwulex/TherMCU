bb69d854269cd41c1ee7f8a54f4980ba0b99254d
Dom <hwulex@gmail.com>
2017-04-25 23:29:27 -0700

    Rotary code cracked! With debug comments

--- trotary.lua
+++ trotary.lua
@@ -2,7 +2,9 @@
 
 tRotary = {
 	pos	= 0
+	, when = 0
 	, channel = 0
+	, debounce = 100
 }
 
 function tRotary.new( config )
@@ -14,15 +16,18 @@ end
 
 function tRotary:init()
 	rotary.setup( self.channel, self.config.pinA, self.config.pinB, self.config.switch )
+	-- print( "Rotary position: " .. rotary.getpos( self.channel ) )
 
 	-- Turn event
 	rotary.on( self.channel, rotary.TURN, function( type, pos, when )
-		print( "Position=" .. pos .. " event type=" .. type .. " time=" .. when )
+		-- print( "Position=" .. pos .. " event type=" .. type .. " time=" .. when )
 
 		-- Read position from rotary to figure out if left or right
-		pos = rotary.getpos( self.channel )
-		print( "Rotary cur: " .. self.pos )
-		print( "Rotary pos: " .. pos )
+		pos = math.floor( rotary.getpos( self.channel ) / 4 )
+		-- print( "Rotary cur: " .. self.pos )
+		-- print( "Rotary pos: " .. pos )
+		-- print( "Rotary when: " .. self.when )
+		-- print( "Rotary now: " .. when )
 
 		-- check rotation
 		if pos < self.pos then
@@ -35,18 +40,20 @@ function tRotary:init()
 			print( "Rotary turned right" )
 		else
 			-- no turn (or end in same place as start)
-			print( "Rotry turned but ended up where we started. Do nothing" )
+			-- print( "Rotary turned but ended up where we started. Do nothing" )
 			return
 		end
+		-- print( "else should not get here" )
 		self.pos = pos
+		self.when = when
 
 		-- Send turn signal to app
-		app:rotary( turn )
+		-- app:rotary( turn )
 	end)
 
 	-- Click event
 	rotary.on( self.channel, rotary.CLICK, function( type )
 		print( "Rotary clicked: event type (" .. type .. ")" )
-		app:rotary( 0 )
+		-- app:rotary( 0 )
 	end)
 end
